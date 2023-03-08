35cb7c3ef5092e06f3fa16f0bfc4db4f3ea172a8
KsenyaM2005 <118298442+KsenyaM2005@users.noreply.github.com>
2023-03-09 02:42:39 +0300

    Update Plan
    Adding functions for completing list with points -> track

--- plan_pair.py
+++ plan_pair.py
@@ -13,52 +13,60 @@ class PairPoints:
 
 
 class Plan:
-    def __init__(self, mat: np.array, lower_limit: int, lst_edges: list): # , lst_no_edges: list
-        # mat: np.array, lower_limit: int, lst_edges: list, lst_no_edges: list
+    def __init__(self, mat: np.array, lower_limit: int, lst_edges: list, track=[]):
         self.mat = mat.copy()
         self.lower_limit = lower_limit
         self.lst_edges = lst_edges.copy()
+        self.track = track.copy()
 
-        # self.lst_not_edges = lst_no_edges.copy()
+        if len(self.lst_edges) > 0:
+            self.do_array_ids()
 
     def __copy__(self):
         return Plan(self.mat.copy(), self.lower_limit,
-                    self.lst_edges.copy())  # , self.lst_not_edges.copy()
+                    self.lst_edges.copy())
 
-    # new
+    # Compare operands:
     def __verify_data(cls, other):
         if not isinstance(other, (int, Plan)):
             raise TypeError("Right operand must have int or Plan type")
         return other if isinstance(other, int) else other.lower_limit
 
-    # new
     def __lt__(self, other):
         return self.lower_limit < self.__verify_data(other)
 
+    def __le__(self, other):
+        return self.lower_limit <= self.__verify_data(other)
+
+    # Other:
     def app(self, edge: PairPoints):
         if edge.is_included():
             self.lst_edges.append(edge)
-        # else: self.lst_not_edges.append(edge)
 
     def inc_limit(self, val: int):
         self.lower_limit += val
 
-    '''def do_array_ids(self):
-        array_ids = []
-        for k in range(len(self.lst_edges)):
-            for pair in self.lst_edges:
-                if len(array_ids) == 0:
-                    array_ids.append(pair.id1)
-                    array_ids.append(pair.id2)
-                    break
-                if array_ids[-1] == pair.id1:
-                    array_ids.append(pair.id2)
-                elif array_ids[0] == pair.id2:
-                    array_ids = [pair.id1] + array_ids
-        return array_ids'''
+    # Functions for completing list with points -> track
+    def add_edge_to_track(self, pair: PairPoints):
+        if len(self.track) == 0:
+            self.track.append(int(pair.id1))
+            self.track.append(int(pair.id2))
+            self.lst_edges.remove(pair)
+        elif self.track[0] == pair.id2:
+            self.track = [int(pair.id1)] + self.track
+            self.lst_edges.remove(pair)
+        elif self.track[-1] == pair.id1:
+            self.track.append(int(pair.id2))
+            self.lst_edges.remove(pair)
 
+    def do_array_ids(self):
+        for pair in self.lst_edges:
+            self.add_edge_to_track(pair)
 
-
+    def make_right_order(self, first_id: int):
+        for i in range(len(self.track)):
+            if self.track[i] == first_id:
+                self.track = self.track[i:] + self.track[:i+1]
 
 # Press the green button in the gutter to run the script.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
