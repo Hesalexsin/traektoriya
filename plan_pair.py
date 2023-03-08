1b0382567fd7794140a21ddad502c2738e5262be
KsenyaM2005 <118298442+KsenyaM2005@users.noreply.github.com>
2023-03-08 23:12:50 +0300

    Update plan_pair.py

--- plan_pair.py
+++ plan_pair.py
@@ -2,50 +2,6 @@ import numpy as np
 from numpy import inf
 
 
-'''class Point:
-    def __init__(self, id_val=0, x_val=0, y_val=0):
-        self.__id = id_val
-        self.x = x_val
-        self.y = y_val
-
-    ''''''def __init__(self, pnt: Point):
-        self.x = pnt.get_x()
-        self.y = pnt.get_y()
-        self.id = pnt.get_id()''''''
-
-    def get_x(self): return self.x
-
-    def get_y(self): return self.y
-
-    def get_id(self): return self.__id
-
-'''
-'''
-class Edge:
-    def __init__(self, start_pnt=Point(), end_pnt=Point()):
-        self.start = Point(start_pnt.get_x(), start_pnt.get_y(), start_pnt.get_id())
-        self.end = Point(end_pnt.get_x(), end_pnt.get_y(), end_pnt.get_id())
-
-    def distance(self):
-        return ((self.start.get_x() - self.end.get_x()) ** 2 +
-                (self.start.get_y() - self.end.get_y()) ** 2) ** 0.5
-
-
-class Edge_alg(Edge):
-    def __init__(self, is_include: bool):
-        super.__init__(self)
-        self.is_include = is_include
-
-    def is_included(self):
-        return self.is_include
-'''
-'''
-
-def distance(pnt1: Point, pnt2: Point) -> float:
-    return ((pnt1.get_x() - pnt2.get_x()) ** 2 +
-            (pnt1.get_y() - pnt2.get_y()) ** 2) ** 0.5
-
-'''
 class PairPoints:
     def __init__(self, id1: int, id2: int, is_include: bool):
         self.id1 = id1
@@ -55,56 +11,54 @@ class PairPoints:
     def is_included(self):
         return self.is_include
 
-    def id_1(self):
-        return self.id1
-
-    def id_2(self):
-        return self.id2
-
 
 class Plan:
-    def __init__(self, mat: np.array, lower_limit: int, lst_edges: list, lst_no_edges: list):
+    def __init__(self, mat: np.array, lower_limit: int, lst_edges: list): # , lst_no_edges: list
+        # mat: np.array, lower_limit: int, lst_edges: list, lst_no_edges: list
         self.mat = mat.copy()
         self.lower_limit = lower_limit
         self.lst_edges = lst_edges.copy()
-        self.lst_not_edges = lst_no_edges.copy()
+
+        # self.lst_not_edges = lst_no_edges.copy()
 
     def __copy__(self):
-        return Plan(self.matrix().copy(), self.lower_limit,
-                    self.lst_edges.copy(), self.lst_not_edges.copy())
+        return Plan(self.mat.copy(), self.lower_limit,
+                    self.lst_edges.copy())  # , self.lst_not_edges.copy()
 
-    def low_limit(self):
-        return self.lower_limit
+    # new
+    def __verify_data(cls, other):
+        if not isinstance(other, (int, Plan)):
+            raise TypeError("Right operand must have int or Plan type")
+        return other if isinstance(other, int) else other.lower_limit
 
-    def matrix(self):
-        return self.mat
-
-    def edges(self):
-        return self.lst_edges
+    # new
+    def __lt__(self, other):
+        return self.lower_limit < self.__verify_data(other)
 
     def app(self, edge: PairPoints):
         if edge.is_included():
             self.lst_edges.append(edge)
-        else:
-            self.lst_not_edges.append(edge)
+        # else: self.lst_not_edges.append(edge)
 
     def inc_limit(self, val: int):
         self.lower_limit += val
 
-    def is_in_lst_not_edges(self, edge: PairPoints):
-        return edge in self.lst_not_edges
+    '''def do_array_ids(self):
+        array_ids = []
+        for k in range(len(self.lst_edges)):
+            for pair in self.lst_edges:
+                if len(array_ids) == 0:
+                    array_ids.append(pair.id1)
+                    array_ids.append(pair.id2)
+                    break
+                if array_ids[-1] == pair.id1:
+                    array_ids.append(pair.id2)
+                elif array_ids[0] == pair.id2:
+                    array_ids = [pair.id1] + array_ids
+        return array_ids'''
 
 
-def is_less(plan1: Plan, plan2: Plan):
-    return plan1.low_limit() < plan2.low_limit()
 
-
-def index_min_plan(lst_plans: list):
-    index, min_limit = -1, inf
-    for i in range(len(lst_plans)):
-        if lst_plans[i].low_limit() < min_limit:
-            index, min_limit = i, lst_plans[i].low_limit()
-    return index
 
 # Press the green button in the gutter to run the script.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
