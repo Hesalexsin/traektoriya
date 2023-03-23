67856d5668bf926d06b850bbfc8203ed4365e2f8
KsenyaM2005 <118298442+KsenyaM2005@users.noreply.github.com>
2023-03-23 21:16:51 +0300

    Update plan_pair.py
    Changed functions for completing list with points (do_array_ids)

--- plan_pair.py
+++ plan_pair.py
@@ -8,23 +8,16 @@ class PairPoints:
         self.id2 = id2
         self.is_include = is_include
 
-    def is_included(self):
-        return self.is_include
-
 
 class Plan:
-    def __init__(self, mat: np.array, lower_limit: int, lst_edges=[], track=[]):
+    def __init__(self, mat: np.array, lower_limit: int, lst_edges={}):
         self.mat = mat.copy()
         self.lower_limit = lower_limit
-        self.lst_edges = lst_edges.copy()
-        self.track = track.copy()
-
-        if len(self.lst_edges) > 0:
-            self.do_array_ids()
+        self.lst_edges = lst_edges.copy()  # {} if lst_edges is None else lst_edges.copy()
 
     def __copy__(self):
         return Plan(self.mat.copy(), self.lower_limit,
-                    self.lst_edges.copy(), self.track.copy())
+                    self.lst_edges.copy())
 
     # Compare operands:
     def __verify_data(cls, other):
@@ -40,35 +33,20 @@ class Plan:
 
     # Other:
     def app(self, edge: PairPoints):
-        if edge.is_included():
-            self.lst_edges.append(edge)
+        if edge.is_include:
+            if not (edge.id1 in self.lst_edges.keys()):
+                self.lst_edges[edge.id1] = edge.id2
 
     def inc_limit(self, val: int):
         self.lower_limit += val
 
-    # Functions for completing list with points -> track
-    def add_edge_to_track(self, pair: PairPoints):
-        if len(self.track) == 0:
-            self.track.append(int(pair.id1))
-            self.track.append(int(pair.id2))
-            self.lst_edges.remove(pair)
-        elif self.track[0] == pair.id2 and self.track[-1] != pair.id1:
-            self.track = [int(pair.id1)] + self.track
-            self.lst_edges.remove(pair)
-        elif self.track[-1] == pair.id1 and self.track[0] != pair.id2:
-            self.track.append(int(pair.id2))
-            self.lst_edges.remove(pair)
-
-    def do_array_ids(self):
-        for i in range(len(self.lst_edges)):
-            for pair in self.lst_edges:
-                self.add_edge_to_track(pair)
-
-    def make_right_order(self, first_id: int):
-        for i in range(len(self.track)):
-            if self.track[i] == first_id:
-                self.track = self.track[i:] + self.track[:i+1]
-                break
-
-# Press the green button in the gutter to run the script.
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Function for completing list with points -> track
+    def do_array_ids(self, id_first: int):
+        key = id_first
+        track = [int(key)]
+        while key in self.lst_edges.keys():
+            track.append(int(self.lst_edges[key]))
+            copy_key = key
+            key = self.lst_edges[key]
+            del self.lst_edges[copy_key]
+        return track
