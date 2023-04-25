bdcc9a0cfbf9ef37366fa3685fead39cd4f2f25c
KsenyaM2005 <118298442+KsenyaM2005@users.noreply.github.com>
2023-04-25 22:58:02 +0300

    Update travelling_salesman_problem.py

--- travelling_salesman_problem.py
+++ travelling_salesman_problem.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import isinf, isnan
 from plan_pair import PairPoints, Plan
 
 import logging
@@ -10,7 +11,8 @@ import logging.config
 logging.basicConfig(
     filename='algorithm.log',
     level=logging.INFO,
-    format='%(levelname)s:%(funcName)s:%(name)s:%(message)s'
+    format='%(levelname)s:%(funcName)s:%(name)s:%(message)s',
+    filemode='w'
 )
 
 
@@ -33,6 +35,11 @@ def reduction(mat: np.array):
     return summ_min
 
 
+# Additional matrix transformation
+def line_transformation(mat: np.array):
+    pass
+
+
 # Finding of the biggest degrees of zeros and choosing of the edge
 def degree_of_the_one_zero(mat: np.array, i: int, j: int):
     return (np.concatenate((mat[1:i, j], mat[i + 1:, j])).min() +
@@ -40,8 +47,12 @@ def degree_of_the_one_zero(mat: np.array, i: int, j: int):
 
 
 def find_degrees_of_zeros(mat: np.array):
+    """
+    :param mat: REDUCED matrix
+    :return: array of 2 INDEXES of zero with the biggest degrees
+    """
     res, i, j = -1, 0, 0
-    index1, index2 = np.where(mat == 0)[0], np.where(mat == 0)[1]
+    index1, index2 = np.where(mat == 0)
     for k in range(1, len(index1)):
         curr = degree_of_the_one_zero(mat, index1[k], index2[k])
         if curr > res:
@@ -50,8 +61,9 @@ def find_degrees_of_zeros(mat: np.array):
 
 
 # Set accordance between indexes og matrix and id
-def arr_index(mat: np.array, edge_id: list) -> list[int]:
-    return [np.where(mat[:, 0] == edge_id[0])[0], np.where(mat[0, :] == edge_id[1])[0]]
+def arr_index(mat: np.array, edge_id: list):
+    index1, index2 = np.where(mat[:, 0] == edge_id[0])[0], np.where(mat[0, :] == edge_id[1])[0]
+    return index1, index2
 
 
 # Branching: exclusion and inclusion of an edge
@@ -61,14 +73,8 @@ def exclude_edge(plan: Plan, i: int, j: int):
     plan.inc_limit(lower_limit)
 
 
-def exclude_edge_limit(plan: Plan, i: int, j: int):
-    exclude_edge(plan, i, j)
-    return plan.lower_limit
-
-
 def include_edge(plan: Plan, i: int, j: int):
-    i_index = arr_index(plan.mat, [plan.mat[0, j], plan.mat[i, 0]])[0]
-    j_index = arr_index(plan.mat, [plan.mat[0, j], plan.mat[i, 0]])[1]
+    i_index, j_index = arr_index(plan.mat, [plan.mat[0, j], plan.mat[i, 0]])
     plan.mat[i_index, j_index] = np.inf
     plan.mat = np.delete(plan.mat, i, 0)
     plan.mat = np.delete(plan.mat, j, 1)
@@ -77,31 +83,24 @@ def include_edge(plan: Plan, i: int, j: int):
     plan.inc_limit(lower_limit)
 
 
-def include_edge_limit(plan: Plan, i: int, j: int):
-    include_edge(plan, i, j)
-    return plan.lower_limit
-
-
 # The function, which helps to avoid infinity cycles in track
 def avoid_cycle(plan: Plan):
     for id1 in plan.lst_edges.keys():
         key = plan.lst_edges[id1]
         while key in plan.lst_edges.keys() and plan.lst_edges[key] != id1:
             id2 = plan.lst_edges[key]
-            index1 = arr_index(plan.mat, [id2, id1])[0]
-            index2 = arr_index(plan.mat, [id2, id1])[1]
+            index1, index2 = arr_index(plan.mat, [id2, id1])
             plan.mat[index1, index2] = np.inf
-            if not(id2 in plan.lst_edges.keys()):
+            if not (id2 in plan.lst_edges.keys()):
                 break
             key = plan.lst_edges[id2]
 
 
 # Comparing current and ragged branches to choosing one with min lower limit
-# ???????????????????????????????????????????????????????????????????????????
 def ragged_branches_compare(ragged_branches: list, current: Plan) -> Plan:
     if len(ragged_branches) > 0:
-        min_i = np.argmin(ragged_branches)  # index_min_plan(ragged_branches)
-        if ragged_branches[min_i] < current:
+        min_i = np.argmin(ragged_branches)
+        if ragged_branches[min_i] < current and ragged_branches[min_i].lower_limit is not np.nan:
             ragged_branches[min_i], current = current, ragged_branches[min_i]
     return current
 
@@ -114,29 +113,24 @@ def neighbour(i: int):
         return 1
 
 
-def find_remaining_edges(current: Plan) -> Plan:
+def find_remaining_edges(current: Plan):
     for i in [1, 2]:
         for j in [1, 2]:
             if current.mat[i, j] == np.inf:
                 current.app(PairPoints(current.mat[i, 0], current.mat[0, neighbour(j)], True))
                 current.app(PairPoints(current.mat[neighbour(i), 0], current.mat[0, j], True))
-    return current
 
 
 # The functions, which does the Little's algorythm
 def do_new_plan(current: Plan, i: int, j: int, is_include: bool):
     branch = current.__copy__()
     branch.app(PairPoints(branch.mat[i, 0], branch.mat[0, j], is_include))
-    if is_include:
-        include_edge(branch, i, j)
-    else:
-        exclude_edge(branch, i, j)
-
+    include_edge(branch, i, j) if is_include else exclude_edge(branch, i, j)
     logging.info('The plan is made')
     return branch
 
 
-def travel_salesman_problem(dist_matrix: np.array):
+def travel_salesman_problem(dist_matrix: np.array, id_airport: int):
     """
     The main function, which realizes the Little's method
     :param dist_matrix: the two-dimensional array (matrix), whose first line and first column consist of IDs of points
@@ -149,16 +143,15 @@ def travel_salesman_problem(dist_matrix: np.array):
              through all points
     """
     ragged_branches = []
-    id_first = dist_matrix[0, 1]
     limit = reduction(dist_matrix)
     current_plan = Plan(dist_matrix, limit)
+    print(current_plan.mat)
     while len(current_plan.mat) > 3:
         edge_index = find_degrees_of_zeros(current_plan.mat)
         i, j = arr_index(current_plan.mat, edge_index)[0][0], arr_index(current_plan.mat, edge_index)[1][0]
 
-        plan_exclude = do_new_plan(current_plan.__copy__(), i, j, is_include=False)
+        plan_exclude = do_new_plan(current_plan, i, j, is_include=False)  # it can NOT copy matrix
         plan_include = do_new_plan(current_plan, i, j, is_include=True)
-
         if plan_include <= plan_exclude:
             current_plan, ragged_branch = plan_include, plan_exclude
         else:
@@ -166,10 +159,8 @@ def travel_salesman_problem(dist_matrix: np.array):
 
         ragged_branches.append(ragged_branch)
         current_plan = ragged_branches_compare(ragged_branches, current_plan)
-        #dist_matrix = current_plan.mat.copy()
+        
+        logging.info('The iteration of the main algorithm is finished')
 
-    logging.info('The iteration of the main algorithm is finished')
-
-    current_plan = find_remaining_edges(current_plan)
-    track = current_plan.do_array_ids(id_first)
-    return track
+    find_remaining_edges(current_plan)
+    return current_plan.do_array_ids(id_airport)
